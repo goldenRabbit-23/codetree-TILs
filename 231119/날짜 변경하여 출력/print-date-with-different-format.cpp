194c511aa1838482f09db74c59db4c9f7598f314
goldenRabbit-23 <115704061+goldenRabbit-23@users.noreply.github.com>
2023-11-19 13:45:20 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231119/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    cin >> y;
+    cin.ignore();
+    cin >> m;
+    cin.ignore();
+    cin >> d;
+    cout << m << '-' << d << '-' << y << '\n';
+
+    return 0;
+}
