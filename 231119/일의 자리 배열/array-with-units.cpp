f15f85cd21828f0c80ec8dc3ec281d78b6ac3287
goldenRabbit-23 <115704061+goldenRabbit-23@users.noreply.github.com>
2023-11-19 14:45:15 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231119/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr(10);
+    cin >> arr[0] >> arr[1];
+    cout << arr[0] << ' ' << arr[1] << ' ';
+
+    for (int i = 2; i < 10; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+        cout << arr[i] % 10 << ' ';
+    }
+
+    return 0;
+}
